8a523cec041227b5a272396c7f036bcd05807fcb
Zachary Lian <zachary.lian@gmail.com>
2022-08-28 03:22:58 +0800

    need pieChart help

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,42 @@
     <div>
         <navigation-bar></navigation-bar>
     </div>
+    <div id="pie-chart">
+        <pie-chart :data="chartData" :options="chartOptions"></pie-chart>
+    </div>
 </template>
 
 <script>
+// import axios from 'axios';
+
 import NavigationBar from './components/layouts/TheNavigationBar.vue';
+import PieChart from './pages/pie-chart';
 
 export default {
     name: 'App',
     components: {
-        'navigation-bar': NavigationBar
+        'navigation-bar': NavigationBar,
+        'pie-chart': PieChart
+    },
+    
+    data() {
+        return {
+            chartOptions: {
+                hoverBorderWidth: 20
+            },
+            chartData: {
+                hoverBackgroundColor: "red",
+                hoverBorderWidth: 10,
+                labels: ["Green", "Red", "Blue"],
+                datasets: [
+                {
+                    label: "Data One",
+                    backgroundColor: ["#41B883", "#E46651", "#00D8FF"],
+                    data: [1, 10, 5]
+                }
+                ]
+            }
+        };
     }
 };
 </script>
