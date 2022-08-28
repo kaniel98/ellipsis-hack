da0595c95dfac0e86f8b32967b44f9ec0a2021d8
L1uM4rt1n <96768015+L1uM4rt1n@users.noreply.github.com>
2022-08-28 12:51:35 +0800

    Create topCategories.vue

---
+++ src/components/ui/topCategories.vue
@@ -0,0 +1,50 @@
+<style>
+
+
+
+</style>
+
+<script>
+      // Transactions Volume 
+      <table>
+            <tr>
+                  <th colspan = '4'> Top Categories (Transaction Volume) </th>
+            </tr>
+
+            <tr>
+                  <th> # </th>
+                  <th> Category </th>
+                  <th> Transaction Volume </th>
+                  <th> Sales Revenue </th>
+            </tr>
+
+            <tr>
+                  <td>  </td>
+                  <td> Household Appliances </td>
+                  <td color = 'mediumspringgreen'> +6000 </td>
+                  <td color = 'mediumspringgreen'> +140000 </td>
+            </tr>
+
+            <tr>
+                  <td >  </td>
+                  <td> Fresh Groceries </td>
+                  <td color = 'mediumspringgreen'> +320 </td>
+                  <td color = 'mediumspringgreen'> +50000 </td>
+            </tr>
+
+            <tr>
+                  <td >  </td>
+                  <td> Fashion Apparels </td>
+                  <td color = 'mediumspringgreen'> +80 </td>
+                  <td color = 'mediumspringgreen'> +3600 </td>
+            </tr>
+
+            <tr>
+                  <td >  </td>
+                  <td> Gym Equipment </td>
+                  <td color = 'red'> -60 </td>
+                  <td color = 'mediumspringgreen'> +2600 </td>
+            </tr>
+      </table>
+
+</script>
