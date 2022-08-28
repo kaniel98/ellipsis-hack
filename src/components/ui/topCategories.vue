5552db5213bc2b42e72a4cedb2c9561ffcd6d946
L1uM4rt1n <96768015+L1uM4rt1n@users.noreply.github.com>
2022-08-28 16:02:47 +0800

    Update topCategories.vue

--- src/components/ui/topCategories.vue
+++ src/components/ui/topCategories.vue
@@ -21,28 +21,28 @@
             </tr>
 
             <tr>
-                  <td> 1 </td>
+                  <th> 1 </th>
                   <td> Household Appliances </td>
                   <td color = 'mediumspringgreen'> +6000 </td>
                   <td color = 'mediumspringgreen'> +140000 </td>
             </tr>
 
             <tr>
-                  <td > 2 </td>
+                  <th> 2 </th>
                   <td> Fresh Groceries </td>
                   <td color = 'mediumspringgreen'> +320 </td>
                   <td color = 'mediumspringgreen'> +50000 </td>
             </tr>
 
             <tr>
-                  <td > 3 </td>
+                  <th> 3 </th>
                   <td> Fashion Apparels </td>
                   <td color = 'mediumspringgreen'> +80 </td>
                   <td color = 'mediumspringgreen'> +3600 </td>
             </tr>
 
             <tr>
-                  <td > 4 </td>
+                  <th> 4 </th>
                   <td> Gym Equipment </td>
                   <td color = 'red'> -60 </td>
                   <td color = 'mediumspringgreen'> +2600 </td>
